d5aafc537708ee46ab3013a28510d71e0dbf2228
Vallee Carey <valleecarey@Vallees-MBP.home>
2020-09-15 23:09:18 -0400

    imported tkinter and created class App1

---
+++ app.py
@@ -0,0 +1,13 @@
+import tkinter as tk 
+from tkinter import Label, Button
+
+
+class App1:
+    def __init__(self, top):
+        self.top = top 
+        top.title("Restaurant Management")
+        top.geometry("1028x500")
+
+root = tk.Tk()
+myGui = App1(root)
+root.mainloop()
