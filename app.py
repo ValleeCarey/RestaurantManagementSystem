634f4f1b92126da6220c79675ac998e7d9078188
Vallee Carey <valleecarey@Vallees-MBP.home>
2020-09-15 23:13:30 -0400

    created a window

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ class App1:
         self.top = top 
         top.title("Restaurant Management")
         top.geometry("1028x500")
+        top.configure(background="#354e71")
 
 root = tk.Tk()
 myGui = App1(root)
